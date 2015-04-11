f50921416440619816fa1048abc67f7f1cd00f7d
Brandon Groff <brandongroff@Brandon-OS-X.dhcp.bsu.edu>
2015-04-10 20:00:55 -0400

    Removed UIDynamicAnimator, sticking to old animation method.

--- Bombnanza/TestingRangeViewController.m
+++ Bombnanza/TestingRangeViewController.m
@@ -7,11 +7,8 @@
 //
 
 #import "TestingRangeViewController.h"
-#import "BombBehavior.h"
 
-@interface TestingRangeViewController () <UIDynamicAnimatorDelegate>
-@property (strong, nonatomic) BombBehavior *bombBehavior;
-@property (strong, nonatomic) UIDynamicAnimator *animator;
+@interface TestingRangeViewController ()
 @property (strong, nonatomic) UIImageView *fallingBomb;
 @property (nonatomic) BOOL timerRepeat;
 @property (nonatomic) float fallRate;
@@ -27,49 +24,28 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    NSString *os =[[NSProcessInfo processInfo] operatingSystemVersionString];
-    float iosVersion  = [[os substringWithRange:NSMakeRange(8, 3)] floatValue];
-    if (iosVersion >= 7.0){
-        [self addBombToView];
-        [self drop];
-    } else {
-        _fallRate = 1.0;
-        _timerRepeat = YES;
-        [self addBombToView];
-        _timer = [NSTimer scheduledTimerWithTimeInterval:(0.03)
-                                                  target:self
-                                                selector:@selector(fall)
-                                                userInfo:nil
-                                                 repeats:self.timerRepeat];
-    }
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (UIDynamicAnimator *)animator
-{
-    if (!_animator) {
-        _animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-        _animator.delegate = self;
-    }
-    return _animator;
-}
-
-- (BombBehavior *)bombBehavior
-{
-    if (!_bombBehavior) {
-        _bombBehavior = [[BombBehavior alloc] init];
-        [self.animator addBehavior:_bombBehavior];
-    }
-    return _bombBehavior;
+    _fallRate = 1.0;
+    _timerRepeat = YES;
+    [self addBombToView];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:(0.03)
+                                              target:self
+                                            selector:@selector(drop)
+                                            userInfo:nil
+                                             repeats:self.timerRepeat];
 }
 
 - (void)drop
 {
-    [self.bombBehavior addItem:self.fallingBomb];
+    float viewBottom = self.fallingBomb.frame.size.height/2 + self.fallingBomb.center.y;
+    if (viewBottom < self.view.frame.size.height){
+        self.fallingBomb.center = CGPointMake(self.fallingBomb.center.x + self.pos.x, self.fallingBomb.center.y + self.pos.y);
+        _pos = CGPointMake(0.0, self.fallRate);
+        _fallRate+= 0.5;
+    } else {
+        _timerRepeat = NO;
+        [self.timer invalidate];
+        //explosion!
+    }
 }
 
 - (void) addBombToView {
@@ -93,19 +69,6 @@
     self.fallingBomb = dropView;
     
     [self.view addSubview:self.fallingBomb];
-}
-
-- (void)fall {
-    float viewBottom = self.fallingBomb.frame.size.height/2 + self.fallingBomb.center.y;
-    if (viewBottom < self.view.frame.size.height){
-        self.fallingBomb.center = CGPointMake(self.fallingBomb.center.x + self.pos.x, self.fallingBomb.center.y + self.pos.y);
-        _pos = CGPointMake(0.0, self.fallRate);
-        _fallRate+= 0.5;
-    } else {
-        _timerRepeat = NO;
-        [self.timer invalidate];
-        //explosion!
-    }
 }
 
 /*
